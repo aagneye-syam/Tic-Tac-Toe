c423ee75ec45ec857fe977d3a1b32ed2271fe132
aagneye-syam <saagneye2003@gmail.com>
2024-05-10 19:54:22 +0530

    two buttons added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './App.css';
+
+import App from './App';
+
 export default function Square() {
-        return <button className="square">X</button>;
+        return (
+          <>
+            <button className="square">X</button>
+            <button className="square">X</button>
+          </>
+        );
       }
+
+      
