2ac21e749836b92c49cedb9e3c1012c2e7e15c1a
aagneye-syam <saagneye2003@gmail.com>
2024-05-11 23:52:29 +0530

    onClick react render added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-function Square({ value }) {
+import { useState } from 'react';
+
+function Square() {
+  const [value, setValue] = useState(null);
+
   function handleClick() {
-    console.log('clicked!');
+    setValue('X');
   }
 
   return (
@@ -10,5 +14,27 @@ function Square({ value }) {
     >
       {value}
     </button>
+  );
+}
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
   );
 }
